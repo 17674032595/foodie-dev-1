1edb1509b577a1219ca2a9fa6de5f920f53a63be
wuyiccc <3406324191@qq.com>
2020-01-13 20:44:34 +0800

    1.删除购物车商品空接口的编写

--- foodie-dev-api/src/main/java/com/wuyiccc/controller/ShopcartController.java
+++ foodie-dev-api/src/main/java/com/wuyiccc/controller/ShopcartController.java
@@ -1,9 +1,11 @@
 package com.wuyiccc.controller;
 
+import com.sun.org.apache.regexp.internal.RE;
 import com.wuyiccc.pojo.bo.ShopcartBO;
 import com.wuyiccc.utils.WUYICCCJSONResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,7 +25,9 @@ public class ShopcartController {
     @PostMapping("/add")
     @ApiOperation(value = "添加商品到购物车",notes = "添加商品到购物车",httpMethod = "POST")
     public WUYICCCJSONResult add(
+            @ApiParam(name = "userId",value = "用户id",required = true)
             @RequestParam String userId,
+            @ApiParam(name = "shopcartBO",value = "购物车的商品BO对象",required = true)
             @RequestBody ShopcartBO shopcartBO,
             HttpServletRequest request,
             HttpServletResponse response
@@ -38,6 +42,31 @@ public class ShopcartController {
         //TODO 前端用户在登录的情况下，添加商品到购物车，会同时在后端同步购物车到redis缓存
         return  WUYICCCJSONResult.ok();
     }
+
+    @ApiOperation(value = "从购物车中删除商品",notes = "从购物车中删除商品",httpMethod = "POST")
+    @PostMapping("/del")
+    public WUYICCCJSONResult del(
+            @ApiParam(name = "userId",value = "用户id",required = true)
+            @RequestParam String userId,
+            @ApiParam(name = "itemSpecId",value = "商品规格id",required = true)
+            @RequestParam String itemSpecId,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ){
+
+        if(StringUtils.isBlank(userId) || StringUtils.isBlank(itemSpecId)){
+            return WUYICCCJSONResult.errorMsg("参数不能为空");
+        }
+
+        //TODO 用户在页面删除购物车中的商品数据,如果此时用户已登录,则需要同步删除后端购物车中的商品 redis
+
+
+        return WUYICCCJSONResult.ok();
+
+
+    }
+
+
 
 
 }
