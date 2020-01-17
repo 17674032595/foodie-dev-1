34c830eef490e20eeecd2c3ceea768f166b2c9f1
wuyiccc <3406324191@qq.com>
2020-01-17 17:07:35 +0800

    1.为静态资源提供网络映射服务

--- foodie-dev-api/src/main/java/com/wuyiccc/config/WebMvcConfig.java
+++ foodie-dev-api/src/main/java/com/wuyiccc/config/WebMvcConfig.java
@@ -4,6 +4,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author wuyiccc
@@ -12,7 +14,18 @@ import org.springframework.web.client.RestTemplate;
  */
 
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+    //实现静态资源的映射
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")//映射所有资源
+                .addResourceLocations("file:/code_learn/java_structure_mooc_bus/images/")//映射本地静态资源
+                .addResourceLocations("classpath:/META-INF/resources/"); //swagger2 的生成的html的地址
+
+
+    }
 
     @Bean// key 默认为当前方法名称
     public RestTemplate restTemplate(RestTemplateBuilder builder){
