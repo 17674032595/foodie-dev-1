07185ee9e2b00ceb0d523c692a73146a077df7da
wuyiccc <3406324191@qq.com>
2020-01-17 11:20:58 +0800

    1.用户中心---使用hibernate 验证用户信息

--- foodie-dev-api/src/main/java/com/wuyiccc/controller/center/CenterUserController.java
+++ foodie-dev-api/src/main/java/com/wuyiccc/controller/center/CenterUserController.java
@@ -10,13 +10,20 @@ import com.wuyiccc.utils.WUYICCCJSONResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.print.attribute.standard.MediaSize;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author wuyiccc
@@ -37,21 +44,42 @@ public class CenterUserController {
     @ApiOperation(value = "修改用户信息", notes = "修改用户信息", httpMethod = "POST")
     @PostMapping("/update")
     public WUYICCCJSONResult update(
-            @ApiParam(name = "userId",value = "用户id",required = true)
+            @ApiParam(name = "userId", value = "用户id", required = true)
             @RequestParam String userId,
-            @RequestBody CenterUserBO centerUserBO,
+            @RequestBody @Valid CenterUserBO centerUserBO,
+            BindingResult result, //存放centerUserBO验证失败的错误信息
             HttpServletRequest request,
             HttpServletResponse response
     ) {
+        if (result.hasErrors()) { //判断是否包含有错误的验证信息
+            Map<String, String> errorMap = getErrors(result);
+            return WUYICCCJSONResult.errorMap(errorMap);
+        }
+
+        if (StringUtils.isBlank(userId)) {
+            return WUYICCCJSONResult.errorMsg("请输入用户id");
+        }
 
         Users user = centerUserService.updateUserInfo(userId, centerUserBO);
         setNullProperty(user);
-        CookieUtils.setCookie(request,response,"user", JsonUtils.objectToJson(user),true);
+        CookieUtils.setCookie(request, response, "user", JsonUtils.objectToJson(user), true);
 
         //TODO 后续要改，增加令牌token，会整合进redis，分布式会话
 
 
         return WUYICCCJSONResult.ok();
+    }
+
+    private Map<String, String> getErrors(BindingResult result) {
+        Map<String, String> map = new HashMap<>();
+        List<FieldError> errorList = result.getFieldErrors();//错误信息列表
+        for (FieldError error : errorList) { //循环每一个错误
+            String errorField = error.getField();//发生错误所对应的属性
+            String errorMsg = error.getDefaultMessage(); // 获取错误内容
+            map.put(errorField,errorMsg); //将错误信息存入map中
+        }
+        return map;
+
     }
 
     /**
--- foodie-dev-pojo/src/main/java/com/wuyiccc/pojo/bo/center/CenterUserBO.java
+++ foodie-dev-pojo/src/main/java/com/wuyiccc/pojo/bo/center/CenterUserBO.java
@@ -2,7 +2,9 @@ package com.wuyiccc.pojo.bo.center;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 
@@ -16,20 +18,35 @@ public class CenterUserBO {
 
     @ApiModelProperty(value="用户名", name="username", example="json", required = false)
     private String username;
+
     @ApiModelProperty(value="密码", name="password", example="123456", required = false)
     private String password;
+
     @ApiModelProperty(value="确认密码", name="confirmPassword", example="123456", required = false)
     private String confirmPassword;
+
+    @NotBlank(message = "用户昵称不能为空") //字段不能为空 + 错误提示消息
+    @Length(max = 12,message = "用户昵称不能超过12位") //最长长度
     @ApiModelProperty(value="用户昵称", name="nickname", example="杰森", required = false)
     private String nickname;
+
+    @Length(max = 12,message = "用户真实姓名不能超过12位") //最长长度
     @ApiModelProperty(value="真实姓名", name="realname", example="杰森", required = false)
     private String realname;
+
+    @Pattern(regexp = "^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\\d{8})$",message = "手机号格式不正确")// 正则表达式
     @ApiModelProperty(value="手机号", name="mobile", example="13999999999", required = false)
     private String mobile;
+
+    @Email //开启email验证
     @ApiModelProperty(value="邮箱地址", name="email", example="imooc@imooc.com", required = false)
     private String email;
+
+    @Min(value = 0,message = "性别选择不正确")//最小值为0
+    @Max(value = 2,message = "性别选择不正确")//最大值为2
     @ApiModelProperty(value="性别", name="sex", example="0:女 1:男 2:保密", required = false)
     private Integer sex;
+
     @ApiModelProperty(value="生日", name="birthday", example="1900-01-01", required = false)
     private Date birthday;
 
